1c6d79948d9218438bbf43e8f4eed71f3fd12f84
Jeremiaszmacura <jeremiaszmacura99@gmail.com>
2022-03-16 08:21:35 +0100

    feature/backend-03 Created simple route to test database model

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 // IMPORT INTERNAL LIBRARIES
+const userRoutes = require('./routes/userRoutes')
 
 // VARIABLES
 const app = express()
@@ -17,6 +18,7 @@ app.use(express.json()) // all data send to api will be able to access as a json
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.use('/users', userRoutes)
 app.use((req, res) => {
   res.status(404).send('404 Error')
 })
