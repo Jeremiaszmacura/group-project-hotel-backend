139c9522dcf6d2906cc3aa198b989d2470c94862
jmacura <jeremiaszmacura99@gmail.com>
2022-03-29 10:48:25 +0200

    bookingController - created few functions

--- backend/src/controllers/bookingController.js
+++ backend/src/controllers/bookingController.js
@@ -1,5 +1,23 @@
+const { User } = require('../models/user')
+const { RoomsCalendar } = require('../models/roomsCalendar')
+const { RoomsCategory } = require('../models/roomsCategory')
+
 const getAll = (req, res) => {
   return res.json('getAll')
+}
+
+const getUserAll = (req, res) => {
+  console.log(req.user)
+  User.findOne({ _id: req.user._id }, (error, data) => {
+    if (error) {
+      console.log(error)
+      return res.json()
+    }
+    if (!data) {
+      return res.json({ error: 'No users in database' })
+    }
+    res.json(data.bookings)
+  })
 }
 
 const getOne = (req, res) => {
@@ -10,8 +28,36 @@ const getBookingsFilter = (req, res) => {
   return res.json('getBookingsFilter')
 }
 
-const createBooking = (req, res) => {
-  return res.json('createBooking')
+const createBooking = async (req, res) => {
+  const roomsCategory = await RoomsCategory.find({}, (error, data) => {
+    if (error) {
+      console.log(error)
+      return res.json()
+    }
+    if (!data) {
+      return res.json({ error: 'There are no categories in database' })
+    }
+  })
+  const allRooms = []
+
+  RoomsCalendar.find({ date: req.body.date }, (error, data) => {
+    if (error) {
+      console.log(error)
+      return res.json()
+    }
+    if (!data) {
+      const roomsJson = {
+        date: req.body.date,
+        rooms: []
+      }
+      for (let i = 0; i < 31; i++) {
+
+      }
+      const roomCalendar = new RoomsCalendar(req.body.date)
+      return res.json({ error: 'No users in database' })
+    }
+    res.json(data.bookings)
+  })
 }
 
 const updateBooking = (req, res) => {
@@ -24,6 +70,7 @@ const removeBooking = (req, res) => {
 
 module.exports = {
   getAll,
+  getUserAll,
   getOne,
   getBookingsFilter,
   createBooking,
--- backend/src/routes/bookingRoutes.js
+++ backend/src/routes/bookingRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const bookingController = require('../controllers/bookingController')
+const { checkIsInRole, isLoggedIn } = require('../middlewares/authMiddleware')
 
 const router = express.Router()
 
 router.get('/', bookingController.getAll)
+router.get('/user/', isLoggedIn, bookingController.getUserAll)
 router.get('/:id', bookingController.getOne)
 router.get('/search/', bookingController.getBookingsFilter)
-router.post('/', bookingController.createBooking)
+router.post('/', isLoggedIn, bookingController.createBooking)
 router.put('/:id', bookingController.updateBooking)
 router.delete('/:id', bookingController.removeBooking)
 
