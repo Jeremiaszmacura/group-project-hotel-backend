7bd811fc46fee91d998122c422ddbba2fbae0464
dkotula <dkotula.wieliczka@gmail.com>
2022-03-30 09:51:01 +0200

    commentController added

--- backend/src/controllers/commentController.js
+++ backend/src/controllers/commentController.js
@@ -1,17 +1,66 @@
+const { Comment } = require('../models/comment')
+
 const getAll = (req, res) => {
-  return res.json('getAll')
+  Comment.find({}, (error, data) => {
+    if (error) {
+      console.log(error)
+      return res.json('something went wrong')
+    }
+    if (!data.length) {
+      return res.json('No comments in database')
+    }
+    return res.json(data)
+  })
 }
 
 const createComment = (req, res) => {
-  return res.json('createComment')
+  req.body.author = req.user._id
+  const comment = new Comment(req.body)
+  comment.save()
+    .then((data) => {
+      res.json(data)
+    })
+    .catch((error) => {
+      console.log(error)
+      return res.json('something went wrong')
+    })
 }
 
 const updateComment = (req, res) => {
-  return res.json('updateComment')
+  Comment.findOne({ _id: req.params.id }, (error, data) => {
+    if (error) {
+      console.log(error)
+      return res.json('something went wrong')
+    }
+    if (!data) {
+      return res.json('No comment in database')
+    }
+    for (const field in req.body) {
+      data[field] = req.body[field]
+    }
+    data.save()
+      .then((data) => {
+        res.json(data)
+      })
+      .catch((error) => {
+        console.log(error)
+        return res.json('something went wrong')
+      })
+  })
 }
 
 const removeComment = (req, res) => {
-  return res.json('removeComment')
+  Comment.findOne({ _id: req.params.id }, (error, data) => {
+    if (error) {
+      console.log(error)
+      return res.json('something went wrong')
+    }
+    if (!data) {
+      return res.json('No comment in database')
+    }
+    data.remove()
+    return res.json('Comment deleted')
+  })
 }
 
 module.exports = {
--- backend/src/routes/commentRoutes.js
+++ backend/src/routes/commentRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const commentController = require('../controllers/commentController')
+const { isLoggedIn } = require('../middlewares/authMiddleware')
 
 const router = express.Router()
 
 router.get('/', commentController.getAll)
-router.post('/', commentController.createComment)
+router.post('/', isLoggedIn, commentController.createComment)
 router.put('/:id', commentController.updateComment)
 router.delete('/:id', commentController.removeComment)
 
