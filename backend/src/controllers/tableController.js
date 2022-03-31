525c08ae04fd29d022bb2d278e3055173dcac44a
Filip Kostkiewicz <ftsk.hj@wp.pl>
2022-03-31 10:51:23 +0200

    Add update, remove tableController

--- backend/src/controllers/tableController.js
+++ backend/src/controllers/tableController.js
@@ -35,12 +35,31 @@ const createTable = (req, res) => {
   return res.json('createTable')
 }
 
-const updateTable = (req, res) => {
-  return res.json('updateTable')
+const updateTable = async (req, res) => {
+  try {
+    const data = await Table.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true, runValidators: true })
+    data ? res.json(data) : res.status(404).send()
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      res.status(422).send(err)
+    } else {
+      res.status(500).send(err)
+    }
+  }
 }
 
 const removeTable = (req, res) => {
-  return res.json('removeTable')
+  Table.findOne({ _id: req.params.id }, (error, data) => {
+    if (error) {
+      console.log(error)
+      return res.json('something went wrong')
+    }
+    if (!data) {
+      return res.json('No table in database')
+    }
+    data.remove()
+    return res.json('Table deleted')
+  })
 }
 
 module.exports = {
